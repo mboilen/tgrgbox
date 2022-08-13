65ebcb8aef5358b8d0ab709ae50b55f12a84618f
mgb <mgb@alumni.brown.edu>
2022-08-13 14:28:48 +0000

    Remove hls and wss subdomains and disable signed policy on playback

--- tgrgbace.tv/webpack.config.js
+++ tgrgbace.tv/webpack.config.js
@@ -61,8 +61,8 @@ function createSources() {
     let viewerPolicyBase64 = base64url(Buffer.from(viewerPolicy, 'utf8'));
 
     var policies = {};
-    makePolicy(policies, 'webrtc', HMAC_KEY, 'wss://wss.' + HOSTNAME + "/tgrgbace/stream", 'ws://wss.' + HOSTNAME + ":3333/tgrgbace/stream", viewerPolicyBase64);
-    makePolicy(policies, 'hls', HMAC_KEY, 'https://hls.' + HOSTNAME + "/tgrgbace/stream/llhls.m3u8", 'http://hls.' + HOSTNAME + ":3333/tgrgbace/stream/llhls.m3u8", viewerPolicyBase64);
+    makePolicy(policies, 'webrtc', HMAC_KEY, 'wss://' + HOSTNAME + "/ome-wss/tgrgbace/stream", 'ws://' + HOSTNAME + ":3333/tgrgbace/stream", viewerPolicyBase64);
+    makePolicy(policies, 'hls', HMAC_KEY, 'https://' + HOSTNAME + "/ome-hls/tgrgbace/stream/llhls.m3u8", 'http://' + HOSTNAME + ":3333/tgrgbace/stream/llhls.m3u8", viewerPolicyBase64);
     //no longer supported
     //makePolicy(policies, 'dash-ll', HMAC_KEY, 'https://hls.' + HOSTNAME + "/tgrgbace/stream/manifest_ll.mpd", 'http://hls.' + HOSTNAME + ":80/tgrgbace/stream/manifest_ll.mpd", viewerPolicyBase64);
 
